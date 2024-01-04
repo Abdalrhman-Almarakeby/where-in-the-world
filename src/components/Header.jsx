999c1d610a86d2b2aca093610c33f48f3986bc54
Abdalrhman-Almarakeby <124980704+Abdalrhman-Almarakeby@users.noreply.github.com>
2024-01-04 11:29:28 -0800

    .

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import moonIcon from "../assets/icons/moon.svg";
 import sunIcon from "../assets/icons/sun.svg";
 import useDarkMode from "../hooks/useDarkMode";
@@ -8,9 +9,12 @@ export default function Header() {
   return (
     <header className="z-50 bg-white text-veryDarkBlue shadow-md dark:bg-blue dark:text-white">
       <div className="container flex items-center justify-between px-6 py-7">
-        <p className="font-extrabold sm:text-sm md:text-lg lg:text-xl xl:text-2xl">
+        <Link
+          to="/"
+          className="font-extrabold sm:text-sm md:text-lg lg:text-xl xl:text-2xl"
+        >
           Where in the world?
-        </p>
+        </Link>
         <button
           onClick={() => setDarkMode(!enabled)}
           className="flex items-center gap-1 text-xs outline-none md:gap-3 md:text-sm"

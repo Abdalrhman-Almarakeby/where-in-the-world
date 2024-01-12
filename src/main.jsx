94ead6255e95e2ec2c865f4f907a0b4904d45cc2
Abdalrhman-Almarakeby <124980704+Abdalrhman-Almarakeby@users.noreply.github.com>
2024-01-12 07:55:14 -0800

    .

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { HashRouter } from "react-router-dom";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/where-in-the-world">
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );

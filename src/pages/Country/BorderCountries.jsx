fa8d379fb8cb0c5c8790e8581fa0721ffe09c5e0
Abdalrhman-Almarakeby <124980704+Abdalrhman-Almarakeby@users.noreply.github.com>
2024-01-14 05:54:27 -0800

    Fix bug with borders

--- src/pages/Country/BorderCountries.jsx
+++ src/pages/Country/BorderCountries.jsx
@@ -3,10 +3,14 @@ import { Link } from "react-router-dom";
 import countriesCode from "../../utils/countriesCode";
 
 export default function BorderCountries({ countries }) {
+  if (countries.includes("ISR") && countries.includes("PSE")) {
+    countries = countries.filter((country) => country !== "ISR");
+  }
 
   if (countries.includes("ISR")) {
-    countries = countries.splice(countries.indexOf("ISR"), 1, "PSE");
+    countries[countries.indexOf("ISR")] = "PSE";
   }
+
   return (
     <div className="flex flex-col gap-4 capitalize text-darkBlue dark:font-semibold dark:text-white sm:gap-6 xl:col-span-2 2xl:col-span-1">
       <p className="flex flex-col text-xl font-extrabold dark:font-semibold sm:text-2xl">
@@ -21,7 +25,7 @@ export default function BorderCountries({ countries }) {
           return (
             <Link
               to={`/country/${countryName}`}
-              className="rounded px-5 py-1 text-sm capitalize shadow-xl dark:bg-blue dark:shadow-lg sm:text-base focus-visible:outline focus-visible:outline-darkBlue dark:focus-visible:outline-white "
+              className="rounded px-5 py-1 text-sm capitalize shadow-xl focus-visible:outline focus-visible:outline-darkBlue dark:bg-blue dark:shadow-lg dark:focus-visible:outline-white sm:text-base"
               key={border}
             >
               {countryName}

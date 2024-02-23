50b5721fc18bb17cd13c7fda79bfec1e3b3eca27
Abdalrhman Almarakeby <aalmarakeby@gmail.com>
2024-02-23 14:30:03 -0800

    .

--- src/pages/Home/Pagination.jsx
+++ src/pages/Home/Pagination.jsx
@@ -52,16 +52,13 @@ export default function Pagination({ nPages, currentPage, setSearchParams }) {
 
       if (currentPage > 3) numbers.push(addDots("A"));
 
-      if (currentPage == numberOfPages)
-        numbers.push(addButton(currentPage - 2));
+      if (currentPage == numberOfPages) numbers.push(addButton(currentPage - 2));
 
       if (currentPage > 2) numbers.push(addButton(currentPage - 1));
 
-      if (currentPage != 1 && currentPage != numberOfPages)
-        numbers.push(addButton(currentPage));
+      if (currentPage != 1 && currentPage != numberOfPages) numbers.push(addButton(currentPage));
 
-      if (currentPage < numberOfPages - 1)
-        numbers.push(addButton(currentPage + 1));
+      if (currentPage < numberOfPages - 1) numbers.push(addButton(currentPage + 1));
 
       if (currentPage == 1) numbers.push(addButton(currentPage + 2));
 

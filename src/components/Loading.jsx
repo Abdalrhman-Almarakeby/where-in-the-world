120784f0c4bf7617434a427a31408d4eccfeae8c
Abdalrhman Almarakeby <aalmarakeby@gmail.com>
2024-05-14 14:07:53 +0300

    Update loading indicator.

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -3,12 +3,14 @@ import LoadingSVG from "../assets/loading.svg?react";
 export default function Loading() {
   return (
     <div
-      className="flex items-center justify-center pt-20"
-      aria-label="Loading indicator"
+      className="flex items-center justify-center pt-20 min-h-[1000px]"
+      aria-live="polite"
+      aria-label="Loading"
     >
       <LoadingSVG
         className="h-24 w-24 fill-darkBlue dark:fill-white"
         role="img"
+        aria-label="Loading indicator"
       />
     </div>
   );

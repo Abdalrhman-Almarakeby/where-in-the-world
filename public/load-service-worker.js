3ad928610f26de180cf0f4ab880f17bbca0c619f
Abdalrhman Almarakeby <aalmarakeby@gmail.com>
2024-06-22 09:02:19 +0300

    Relocate service worker files.

--- public/load-service-worker.js
+++ public/load-service-worker.js
@@ -1,6 +1,6 @@
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker
-    .register("/service-worker/service-worker.js")
+    .register("/service-worker.js")
     .then(() => console.log("service worker registered successfully!"))
     .catch(console.error);
 }

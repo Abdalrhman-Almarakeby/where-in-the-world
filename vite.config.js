d5ead961fbcff6de38ff7941e3b6fcc7b497fd35
Abdalrhman Almarakeby <almarakeby.work@gmail.com>
2025-04-09 23:37:45 +0200

    chore: update .gitignore and add VSCode settings for Biome integration

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react(), tailwindcss()],
-  server: {
-    port: 3000,
-  },
+	server: {
+		port: 3000,
+	},
 });

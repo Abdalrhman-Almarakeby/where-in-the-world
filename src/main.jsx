1572cd0dfa31a284fe410627dd014be3bde08d7d
Abdalrhman Almarakeby <almarakeby.work@gmail.com>
2025-04-10 10:31:26 +0200

    feat: integrate react-router for routing support

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router";
 import { App } from "./App.jsx";
 import "./styles.css";
 
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <App />,
+	},
+]);
+
 createRoot(document.getElementById("root")).render(
 	<StrictMode>
-		<App />
+		<RouterProvider router={router} />
 	</StrictMode>,
 );

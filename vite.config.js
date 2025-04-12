445d70528c1b84112b131ac6430656e53bd467c4
Abdalrhman Almarakeby <almarakeby.work@gmail.com>
2025-04-13 00:25:34 +0200

    chore: format

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,8 @@ import { defineConfig } from "vite";
 import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
-  plugins: [svgr(), react(), tailwindcss()],
-  server: {
-    port: 3000,
-  },
+	plugins: [svgr(), react(), tailwindcss()],
+	server: {
+		port: 3000,
+	},
 });
-

2bec8365bda5194ce2a510e1a4504753124919bd
Abdalrhman Almarakeby <almarakeby.work@gmail.com>
2025-04-13 00:21:14 +0200

    feat: add tailwindcss plugin to Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
-	plugins: [svgr(), react()],
-	server: {
-		port: 3000,
-	},
+  plugins: [svgr(), react(), tailwindcss()],
+  server: {
+    port: 3000,
+  },
 });
+

7fa47e64fe68cc74443338cad641e27f6006b92f
Abdalrhman Almarakeby <almarakeby.work@gmail.com>
2025-05-03 15:49:08 +0300

    feat: add path alias resolution in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+import path from "node:path";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
@@ -6,5 +7,10 @@ export default defineConfig({
 	plugins: [react(), tailwindcss()],
 	server: {
 		port: 3000,
+	},
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
 	},
 });

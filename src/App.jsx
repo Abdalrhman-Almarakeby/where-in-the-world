1fb86376e15e8a778adca3aac04055fc2b4a75b9
Abdalrhman Almarakeby <almarakeby.work@gmail.com>
2025-05-07 06:00:46 +0300

    refactor: clean up imports in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
-import { Outlet, RouterProvider, createBrowserRouter } from "react-router";
-import "@/styles.css";
-
 import { Footer } from "@/components/Footer.jsx";
 import { Header } from "@/components/Header";
 import { Country } from "@/pages/Country/index";
 import { ErrorPage } from "@/pages/Error";
 import { Home } from "@/pages/Home/index";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
-// Root layout componenimport { createRoot } from "react-dom/client";
+import { Outlet, RouterProvider, createBrowserRouter } from "react-router";
+import "@/styles.css";
 
 const Root = () => {
 	return (

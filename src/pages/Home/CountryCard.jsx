96393faffb161dce3ddaa55abb836c689dcd08ba
Abdalrhman Almarakeby <almarakeby.work@gmail.com>
2025-05-08 09:51:47 +0300

    refactor: add JSDocs to CountryCard

--- src/pages/Home/CountryCard.jsx
+++ src/pages/Home/CountryCard.jsx
@@ -3,8 +3,20 @@ import * as countries from "country-flag-icons/react/3x2";
 import PropTypes from "prop-types";
 import { Link } from "react-router";
 
+/**
+ * Displays a card with basic information about a country
+ *
+ * @param {{
+ *   name: { common: string },
+ *   flags: { alt?: string },
+ *   population: number,
+ *   region: string,
+ *   capital?: string[],
+ *   cca2: string
+ * }} props - Country data used to render the card.
+ */
 export function CountryCard({
-	name,
+	name: { common: name },
 	population,
 	flags,
 	region,
@@ -12,25 +24,26 @@ export function CountryCard({
 	cca2,
 }) {
 	const Flag = countries[cca2];
+
 	return (
 		<Link
-			to={`/country/${toKebabCase(name.common)}`}
+			to={`/country/${toKebabCase(name)}`}
 			className="flex h-full min-w-fit flex-col justify-between overflow-hidden rounded-sm bg-white shadow-xl focus:outline-2 focus:outline-dark-blue dark:bg-blue dark:focus:outline-white"
-			title={name.common}
-			aria-label={`Country Card: ${name.common}`}
+			title={name}
+			aria-label={`Country Card: ${name}`}
 		>
 			<Flag
 				role="img"
-				alt={flags.alt || `${name.common} flag`}
-				aria-label={`Flag of ${name.common}`}
+				alt={flags.alt || `${name} flag`}
+				aria-label={`Flag of ${name}`}
 				className="aspect-3/2"
 			/>
 			<div className="p-5 pb-10">
 				<h2
 					className="py-3 text-xl font-bold"
-					aria-label={`Country Name: ${name.common}`}
+					aria-label={`Country Name: ${name}`}
 				>
-					{name.common}
+					{name}
 				</h2>
 				<p className="font-semibold">
 					Population:{" "}
@@ -42,7 +55,7 @@ export function CountryCard({
 				<p className="font-semibold">
 					Capital:{" "}
 					<span className="font-normal">
-						{name.common === "Palestine"
+						{name === "Palestine"
 							? "Jerusalem"
 							: capital?.join(", ") || "No data"}
 					</span>
@@ -57,7 +70,6 @@ CountryCard.propTypes = {
 		common: PropTypes.string.isRequired,
 	}).isRequired,
 	flags: PropTypes.shape({
-		svg: PropTypes.string.isRequired,
 		alt: PropTypes.string,
 	}).isRequired,
 	population: PropTypes.number.isRequired,

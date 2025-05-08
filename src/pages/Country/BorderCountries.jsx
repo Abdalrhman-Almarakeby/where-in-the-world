c2fd7ab4b1578e0918e74648fa67b9f00a4a70fa
Abdalrhman Almarakeby <almarakeby.work@gmail.com>
2025-05-08 09:39:35 +0300

    refactor: remove unnecessary comments from BorderCountries component

--- src/pages/Country/BorderCountries.jsx
+++ src/pages/Country/BorderCountries.jsx
@@ -6,13 +6,6 @@ import { Link } from "react-router";
 /**
  * Renders a list of neighboring countries as navigable links.
  *
- * Special logic is applied to handle the presence of both "ISR" (Israel) and "PSE" (Palestine):
- * - If both are present, "ISR" is removed.
- * - If only "ISR" is present, it is replaced with "PSE".
- *
- * Each country code is mapped to its name using the `countriesCode` map,
- * and displayed as a capitalized link.
- *
  * @param {{ countries: string[] }} props - An array of country codes representing border countries.
  */
 export function BorderCountries({ countries }) {

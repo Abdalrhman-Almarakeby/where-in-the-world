74f2732a2f9068643dd2b06a57eadef29c756d69
Abdalrhman Almarakeby <almarakeby.work@gmail.com>
2025-05-08 11:00:01 +0300

    refactor: update return type in useCountry hook to use Country type

--- src/hooks/useCountry.js
+++ src/hooks/useCountry.js
@@ -6,7 +6,7 @@ import { useQuery } from "@tanstack/react-query";
  * Custom hook to fetch data for a specific country.
  *
  * @param {string} countryName - The name of the country to fetch data for in kebab case.
- * @returns {import('@tanstack/react-query').UseQueryResult<Object, Error>} - React Query result containing the country data.
+ * @returns {import('@tanstack/react-query').UseQueryResult<import("@/types").Country, Error>} - React Query result containing the country data.
  */
 export function useCountry(countryName) {
 	return useQuery({

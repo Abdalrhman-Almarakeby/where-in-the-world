121d792375d19c2aa0762033fb9eb525902d0dbf
Abdalrhman Almarakeby <almarakeby.work@gmail.com>
2025-05-08 09:30:25 +0300

    feat: add manual chunking for vendor dependencies in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -18,4 +18,15 @@ export default defineConfig({
 			"@": path.resolve(__dirname, "./src"),
 		},
 	},
+	build: {
+		rollupOptions: {
+			output: {
+				manualChunks(id) {
+					if (id.includes("node_modules")) {
+						return "vendor";
+					}
+				},
+			},
+		},
+	},
 });

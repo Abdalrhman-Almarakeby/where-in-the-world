c7a86a75737462a4feddbd554c8aae063ee0b66e
Abdalrhman Almarakeby <almarakeby.work@gmail.com>
2025-05-08 08:58:18 +0300

    refactor: move query client initialization to a separate file

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,8 @@
+import { queryClient } from "@/lib/queryClient";
 import { router } from "@/lib/router";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider } from "react-router";
 import "./styles.css";
-
-const queryClient = new QueryClient({
-	defaultOptions: {
-		queries: {
-			staleTime: Number.POSITIVE_INFINITY,
-			refetchOnMount: false,
-			refetchOnWindowFocus: false,
-			refetchOnReconnect: false,
-		},
-	},
-});
 
 export function App() {
 	return (
---
+++ src/lib/queryClient.js
@@ -0,0 +1,12 @@
+import { QueryClient } from "@tanstack/react-query";
+
+export const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			staleTime: Number.POSITIVE_INFINITY,
+			refetchOnMount: false,
+			refetchOnWindowFocus: false,
+			refetchOnReconnect: false,
+		},
+	},
+});

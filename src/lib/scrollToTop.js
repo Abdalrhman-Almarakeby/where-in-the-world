6c82a2f38ab2ddc8f59578dcd62710e7553c6187
Abdalrhman Almarakeby <almarakeby.work@gmail.com>
2025-05-08 10:10:22 +0300

    refactor: enhance JSDoc for scrollToTop function and improve default behavior handling

--- src/lib/scrollToTop.js
+++ src/lib/scrollToTop.js
@@ -1,4 +1,9 @@
-export function scrollToTop(behavior) {
+/**
+ * Scrolls the window to the top with smooth or instant behavior, depending on user preference.
+ *
+ * @param {"auto" | "instant" | "smooth"} behavior - The scroll behavior. Optional parameter.
+ */
+export function scrollToTop(behavior = "auto") {
 	const preferReducedMotion = window.matchMedia(
 		"(prefers-reduced-motion)",
 	).matches;
@@ -6,6 +11,11 @@ export function scrollToTop(behavior) {
 	window.scrollTo({
 		top: 0,
 		left: 0,
-		behavior: behavior || preferReducedMotion ? "instant" : "smooth",
+		behavior:
+			behavior === "auto"
+				? preferReducedMotion
+					? "instant"
+					: "smooth"
+				: behavior,
 	});
 }

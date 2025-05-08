2259b9a913e826ca2625e452d3c2b266d0a251a5
Abdalrhman Almarakeby <almarakeby.work@gmail.com>
2025-05-08 08:08:22 +0300

    feat: replace getUrl utility with direct API_URL usage in useCountry hook

--- src/hooks/useCountry.js
+++ src/hooks/useCountry.js
@@ -1,11 +1,14 @@
-import { getUrl } from "@/utils/getUrl";
+import { API_URL } from "@/constant";
+import { countriesCode } from "@/utils/countriesCode";
 import { useQuery } from "@tanstack/react-query";
 
 export function useCountry(countryName) {
 	return useQuery({
 		queryKey: ["country", countryName],
 		queryFn: async () => {
-			const res = await fetch(getUrl(countryName));
+			const res = await fetch(
+				`${API_URL}/alpha/${countriesCode[countryName]}?fields=name,region,subregion,capital,population,currencies,languages,tld,borders,flags,maps,cca2`,
+			);
 
 			if (!res.ok) {
 				throw new Error("Country not found");
--- src/utils/getUrl.js
+++
@@ -1,9 +0,0 @@
-import { API_URL } from "@/constant";
-import { countriesCode } from "./countriesCode";
-
-export function getUrl(name) {
-	const fields =
-		"?fields=name,region,subregion,capital,population,currencies,languages,tld,borders,flags,maps,cca2";
-
-	return `${API_URL}/alpha/${countriesCode[name]}${fields}`;
-}

187a8147c02bf7811500b5e227bfb170bba02d79
Abdalrhman Almarakeby <almarakeby.work@gmail.com>
2025-05-08 10:42:49 +0300

    refactor: remove unnecessary fields from API endpoint in useCountries hook

--- src/hooks/useCountries.js
+++ src/hooks/useCountries.js
@@ -13,7 +13,7 @@ export function useCountries({ region, search }) {
 	return useQuery({
 		queryKey: ["countries", { region, search }],
 		queryFn: async () => {
-			const endpoint = `${API_URL}/all?fields=name,capital,flags,languages,region,population,cca3,cca2`;
+			const endpoint = `${API_URL}/all?fields=name,capital,flags,region,population,cca2`;
 
 			const response = await fetch(endpoint);
 
